f87a9c704eb9f7d0c5c276b27b0648bb21620cf2
Jacob <jacob.hallberg@colorado.edu>
2019-11-23 11:41:29 -0700

    Galaxy background, with star component implemented.

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import StarfieldAnimation from 'react-starfield-animation'
+
+import galaxy from './westerlund2.jpg'
+
+
+class App extends Component {
+	render() {
+		return (
+			<div
+				style={{
+					background: `url(${galaxy})`,
+					backgroundSize: 'cover',
+					minHeight: '100vh',
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center'
+				}}
+			>
+
+				<StarfieldAnimation
+					numParticles={400}
+					depth={300}
+					style={{
+						position: 'absolute',
+						zIndex: 1,
+						top: 0,
+						left: 0,
+						right: 0,
+						bottom: 0
+					}}
+				/>
+			</div>
+		)
+	}
+}
+
+export default App;
