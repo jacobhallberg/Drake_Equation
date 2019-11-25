0b588fe060610ef989393b8e565e57abbaa13b14
Jacob <jacob.hallberg@colorado.edu>
2019-11-25 15:37:02 -0700

    Added sliders to each variable. No state logic tho.

--- src/components/variable/variable.js
+++ src/components/variable/variable.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react'
 import ReactTooltip from 'react-tooltip'
+import Tooltip from 'rc-tooltip'
+import Slider, { Range } from 'rc-slider'
 import './variable.css'
+import 'rc-slider/assets/index.css';
+
+const Handle = Slider.Handle;
+
+const handle = (props) => {
+  const { value, dragging, index, ...restProps } = props;
+  return (
+    <Tooltip
+      prefixCls="rc-slider-tooltip"
+      overlay={value}
+      visible={true}
+      placement="bottom"
+      key={index}
+    >
+      <Handle value={value} {...restProps} />
+    </Tooltip>
+  );
+};
 
 class Variable extends Component {
 	render() {
@@ -14,7 +34,9 @@ class Variable extends Component {
 					<ReactTooltip id={this.props.variable}>
 						<p> {this.props.variable_info} </p>
 					</ReactTooltip>
+					
 				</div>
+				<Slider min={0} max={20} defaultValue={3} handle={handle} className={"slider"}/>
 			</td>
 		)
 	}
