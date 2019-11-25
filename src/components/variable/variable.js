ccee5d0fa94e41c651a4bf2554717bf3a4744ed1
Jacob <jacob.hallberg@colorado.edu>
2019-11-25 16:26:52 -0700

    Added test images to variable components.

--- src/components/variable/variable.js
+++ src/components/variable/variable.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 import ReactTooltip from 'react-tooltip'
 import Tooltip from 'rc-tooltip'
-import Slider, { Range } from 'rc-slider'
+import Slider from 'rc-slider'
 import './variable.css'
 import 'rc-slider/assets/index.css';
+import test from "../../assets/images/test.jpg"
 
 const Handle = Slider.Handle;
 
@@ -13,7 +14,7 @@ const handle = (props) => {
     <Tooltip
       prefixCls="rc-slider-tooltip"
       overlay={value}
-      visible={true}
+      visible={dragging}
       placement="bottom"
       key={index}
     >
@@ -34,9 +35,7 @@ class Variable extends Component {
 					<p> {this.props.variable_info} </p>
 				</ReactTooltip>
 
-				<div class="circle">
-					
-				</div>
+				<img class="circle" src={test} alt={this.props.variable}/>
 				<Slider min={0} max={20} defaultValue={3} handle={handle} className={"slider"}/>
 			</td>
 		)
