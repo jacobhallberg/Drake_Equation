be6a805e8c128bccc3c0729febd565f3851d87d5
Landon <laba2346@colorado.edu>
2019-11-30 12:20:37 -0700

    Add header, fix CSS

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import StarfieldAnimation from 'react-starfield-animation';
 import EquationBlock from '../equationBlock/equationBlock';
 import galaxy from '../../assets/images/westerlund2.jpg';
-
+import Header from '../header/header';
 
 class App extends Component {
 	render() {
@@ -12,9 +12,10 @@ class App extends Component {
 					background: `url(${galaxy})`,
 					backgroundSize: 'cover',
 					minHeight: '100vh',
-					display: 'flex',
-					justifyContent: 'center',
-					alignItems: 'center'
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    paddingTop: '10px',
+                    zIndex: -2,
 				}}
 			>
 
@@ -23,14 +24,15 @@ class App extends Component {
 					depth={300}
 					style={{
 						position: 'absolute',
-						zIndex: 1,
+						zIndex: 0,
 						top: 0,
 						left: 0,
 						right: 0,
 						bottom: 0
-					}}
+                    }}
 				/>
 
+                <Header></Header>
                 <EquationBlock></EquationBlock>
 			</div>
 		)
