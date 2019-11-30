e2fe6491ab310cd9b01b53ce7d88132864230f13
Landon <laba2346@colorado.edu>
2019-11-30 13:09:29 -0700

    Forgot this

---
+++ src/components/equationBlock/variable_obj.js
@@ -0,0 +1,11 @@
+class VariableObj {
+    constructor(label, info, min, max, defaultValue) {
+        this.label = label
+        this.info = info
+        this.min = min 
+        this.max = max 
+        this.defaultValue = defaultValue
+    }
+}
+
+export default VariableObj
